f037d08af828924bf50b38d8c29f039495264842
Eric Hanko <eric.hanko1@gmail.com>
2018-01-26 22:40:01 -0800

    chef-solo provisioner + macos-cookbook

--- prepare_iso/prepare_pvm.sh
+++ prepare_iso/prepare_pvm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 usage() {
-	cat <<EOF
+  cat <<EOF
 Usage:
 $(basename "$0") "/path/to/diskimage.vhd"
 
@@ -13,18 +13,18 @@ EOF
 
 cleanup() {
   if [ -n "$VM" ] && prlctl list --all | grep -q "$VM"; then
-    prlctl unregister "$VM" > /dev/null
+    prlctl unregister "$VM"
   fi
 }
 
-trap cleanup EXIT INT TERM
+# trap cleanup EXIT INT TERM
 
 msg_status() {
-	echo "\033[0;32m-- $1\033[0m"
+  echo "\\033[0;32m-- $1\\033[0m"
 }
 
 msg_error() {
-	echo "\033[0;31m-- $1\033[0m"
+  echo "\\033[0;31m-- $1\\033[0m"
 }
 
 render_template() {
@@ -33,39 +33,46 @@ render_template() {
 
 if [ ! -f "$1" ]; then
   usage
-	exit 1
+  exit 1
 fi
 
 HARDDRIVE="$1"
 VM="$(basename "${HARDDRIVE%.vhd}")"
 
 OUTPUT="${HARDDRIVE%.vhd}.pvm"
-ABS_PATH="$(realpath ${OUTPUT})"
+ABS_PATH="$(realpath "$OUTPUT")"
 
-ABS_PARENT="$(dirname ${ABS_PATH})"
+ABS_PARENT="$(dirname "$ABS_PATH")"
 CONVERTED_HDD="${ABS_PATH}/${VM}.hdd"
 PARALLELS_HDD="${ABS_PATH}/Macintosh.hdd"
 
-msg_status "Creating a new Parallels virtual machine: ${VM}"
-prlctl create "$VM" --distribution macosx --no-hdd --dst="${ABS_PARENT}" > /dev/null
+msg_status "Creating a new Parallels virtual machine: $VM"
+prlctl create "$VM" --distribution macosx --no-hdd --dst="$ABS_PARENT"
 
 msg_status "Converting VHD to Parallels format"
-prl_convert "$HARDDRIVE" --dst="${OUTPUT}" --allow-no-os
-mv $CONVERTED_HDD $PARALLELS_HDD
+prl_convert "$HARDDRIVE" --dst="$OUTPUT" --allow-no-os
+mv "$CONVERTED_HDD" "$PARALLELS_HDD"
 
 msg_status "Compacting $PARALLELS_HDD"
 prl_disk_tool compact --hdd "$PARALLELS_HDD"
 
 msg_status "Adding SATA Controller and attaching Parallels HDD"
-prlctl set "$VM" --device-add hdd --image "$PARALLELS_HDD" --iface sata --position 0 > /dev/null
+prlctl set "$VM" --device-add hdd --image "$PARALLELS_HDD" --iface sata --position 0
 
 msg_status "Setting up Parallels virtual machine"
-prlctl set "$VM" --efi-boot "on" > /dev/null
-prlctl set "$VM" --cpus "2" > /dev/null
-prlctl set "$VM" --memsize "4096" > /dev/null
+prlctl set "$VM" --efi-boot "on"
+prlctl set "$VM" --cpus "2"
+prlctl set "$VM" --memsize "4096"
+prlctl set "$VM" --memquota "512:2048"
+prlctl set "$VM" --3d-accelerate "highest"
+prlctl set "$VM" --high-resolution "off"
+prlctl set "$VM" --auto-share-camera "off"
+prlctl set "$VM" --auto-share-bluetooth "off"
+prlctl set "$VM" --on-window-close "keep-running"
+prlctl set "$VM" --shf-host "off"
 
-msg_status "Installing Parallels tools"
-prlctl installtools "$VM"
+#msg_status "Installing Parallels tools"
+#prlctl installtools "$VM"
 
 cleanup
 
